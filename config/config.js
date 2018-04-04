764908a72f54a5b32291f9175af81e889cef4b08
이상훈 <sanghun@isanghun-ui-MacBook-Pro.local>
2018-04-04 17:07:13 +0900

    ...

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ const fetchServerConfig = {
   // ip: 'localhost',
   // ip: '13.124.115.77',
   // server
-  // ip: '13.125.234.197',
+  ip: 'http://13.125.234.197',
   // client
-  ip: 'www.peopet.co.kr'
+  // ip: 'www.peopet.co.kr'
 }
 
 const config = {
