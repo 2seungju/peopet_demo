003cb68e23b7ac83c1015dc7406eb53b72b0b9a7
이상훈 <sanghun@isanghun-ui-MacBook-Pro.local>
2018-04-10 18:18:43 +0900

    livere test

--- pages/breederdetail.js
+++ pages/breederdetail.js
@@ -312,6 +312,25 @@ export default class Breederdetail extends Component {
                 <Description>{question_mind}</Description>
                 <Description>{question_word}</Description>
               </InterviewWrapper>
+              {/* <!-- 라이브리 시티 설치 코드 --> */}
+              <div id="lv-container" data-id="city" data-uid="MTAyMC8zNTYxNy8xMjE1Mw==">
+                <script
+                  type="text/javascript"
+                  dangerouslySetInnerHTML={{
+                    __html: `{
+                      (function(d, s) {
+                        var j, e = d.getElementsByTagName(s)[0];
+                        if (typeof LivereTower === 'function') { return; }
+                        j = d.createElement(s);
+                        j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
+                        j.async = true;
+                        e.parentNode.insertBefore(j, e);
+                      })(document, 'script');
+                    }`
+                  }}
+                />
+
+              </div>
               {/* <Bar borderColor={warmGrey2} />
               <KannelInfoWrapper>
                 <Title>견사 정보</Title>
