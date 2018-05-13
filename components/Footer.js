8af7c98e487a6d2328c9215ece08a558eb57c7a8
이상훈 <sanghun@isanghun-ui-MacBook-Pro.local>
2018-05-14 00:13:10 +0900

    동물판매업;

--- components/Footer.js
+++ components/Footer.js
@@ -59,6 +59,7 @@ const Footer = ({ isSideMenuFolded }) => (
     <Content>
       <Text>사업자등록번호 354-88-00899 | 법인명(주)코스모스이펙트 | 대표 최현일</Text>
       <Text>통신판매업 신고 2017-서울강남-04764호 | 주소 서울시 강남구 테헤란로 69길 5, 12층 </Text>
+      <Text>동물판매업 신고 110111-6571388 | 법인명(주)코스모스이펙트 | 대표 최현일</Text>
       <FooterLogo mobileOpen />
       <IconWrapper>
         <IconLink href="https://www.facebook.com/peopleNpet/">
