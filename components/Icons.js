7b58030cdeb2bac10a6e2d9ad436d68c7d28f8e4
이승주 <iseungju@iseungjuui-MacBook-Pro.local>
2018-05-23 16:23:22 +0900

    search img

--- components/Icons.js
+++ components/Icons.js
@@ -56,6 +56,9 @@ const SearchImg = styled.img`
   position: absolute;
   top: ${rem(20)};
   right: ${rem(30)};
+  ${media.tablet`
+    display:none;
+  `};
 `
 
 const ChangeRotationImg = styled.img`
